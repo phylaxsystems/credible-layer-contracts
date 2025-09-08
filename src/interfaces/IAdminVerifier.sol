b603f235b30cfe3f457632fb9ae592a31bb76fbc
fredo <luehrs.fred@gmail.com>
2025-09-08 20:40:08 +0200

    change pragmas

--- src/interfaces/IAdminVerifier.sol
+++ src/interfaces/IAdminVerifier.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CC0-1.0
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.19;
 
 interface IAdminVerifier {
     function verifyAdmin(address contractAddress, address requester, bytes calldata data)
--- test/utils/ProxyHelper.t.sol
+++ test/utils/ProxyHelper.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: CC0-1.0
-pragma solidity ^0.8.28;
+pragma solidity ^0.8.19;
 
 import {Test} from "forge-std/Test.sol";
 import {ProxyAdmin} from "@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol";
