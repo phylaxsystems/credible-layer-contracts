cc1f8acb7268941719d2e32086a3ec6f3833db0f
Léa Narzis <78718413+lean-apple@users.noreply.github.com>
2026-01-07 18:17:17 +0100

    chore: rename const

--- script/DeployCoreWithStaging.s.sol
+++ script/DeployCoreWithStaging.s.sol
@@ -5,11 +5,11 @@ import {DeployCore} from "./DeployCore.s.sol";
 import {console2} from "forge-std/console2.sol";
 
 // Address used to deploy the assertion contract to the forked db, and to call assertion functions.
-address constant CALLER = 0x00000000000000000000000000000000000001a4;
+address constant CALLER_ADDRESS = 0x00000000000000000000000000000000000001a4;
 
 // Fixed address of the assertion contract is used to deploy assertion contracts.
 // Deploying assertion contracts via the caller address @ nonce 0 results in this address.
-address constant ASSERTION_CONTRACT = 0x63F9abBE8aA6Ba1261Ef3B0CBfb25A5Ff8eEeD10;
+address constant ASSERTION_CONTRACT_ADDRESS = 0x63F9abBE8aA6Ba1261Ef3B0CBfb25A5Ff8eEeD10;
 
 // Precompile address
 address constant PRECOMPILE_ADDRESS = 0x4461812e00718ff8D80929E3bF595AEaaa7b881E;
@@ -50,8 +50,8 @@ contract DeployCoreWithStaging is DeployCore {
     }
 
     function _fundPersistentAccounts() internal {
-        _fundIfEmpty(CALLER, "CALLER");
-        _fundIfEmpty(ASSERTION_CONTRACT, "ASSERTION_CONTRACT");
+        _fundIfEmpty(CALLER_ADDRESS, "CALLER_ADDRESS");
+        _fundIfEmpty(ASSERTION_CONTRACT_ADDRESS, "ASSERTION_CONTRACT_ADDRESS");
         _fundIfEmpty(PRECOMPILE_ADDRESS, "PRECOMPILE_ADDRESS");
     }
 
